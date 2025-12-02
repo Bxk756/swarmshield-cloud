c113bf31cb5a476c77584d3c00a309418ee3eb67
Brian Kelley <225787637+Bxk756@users.noreply.github.com>
2025-12-02 06:02:03 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}", "./styles/**/*.css"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./styles/**/*.css"
+  ],
   darkMode: "class",
   theme: {
     extend: {
